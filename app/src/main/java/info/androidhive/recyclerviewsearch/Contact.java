2a54b5a2608fb80d5626b2f81cccde606ef53b3c
ramdan riawan <ramdanriawan3@gmail.com>
2019-01-06 19:45:24 +0700

    Initial commit

---
+++ app/src/main/java/info/androidhive/recyclerviewsearch/Contact.java
@@ -0,0 +1,22 @@
+package info.androidhive.recyclerviewsearch;
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
